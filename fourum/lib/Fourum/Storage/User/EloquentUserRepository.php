6eda985a5158ef2863b9d6b0fc0cebb63f18e67a
Will Pillar <will.pillar@graze.com>
2013-08-28 23:00:59 +0100

    Adding doc to EloquentUserRepository and type hinting for an array on create()

--- fourum/lib/Fourum/Storage/User/EloquentUserRepository.php
+++ fourum/lib/Fourum/Storage/User/EloquentUserRepository.php
@@ -2,19 +2,40 @@
 
 use Fourum\Models\User;
 
+/**
+ * Eloquent User Repository
+ *
+ * User Repository for an Eloquent model.
+ */
 class EloquentUserRepository implements UserRepositoryInterface
 {
+    /**
+     * Return all Users
+     *
+     * @return \Illuminate\Database\Eloquent\Collection
+     */
     public function all()
     {
         return User::all();
     }
 
+    /**
+     * Find User(s)
+     *
+     * @param  integer $id
+     * @return \Illuminate\Database\Eloquent\Model|Collection
+     */
     public function get($id)
     {
         return User::find($id);
     }
 
-    public function create($input)
+    /**
+     * Create User
+     * @param  array $input
+     * @return \Illuminate\Database\Eloquent\Model
+     */
+    public function create(array $input)
     {
         return User::create($input);
     }
