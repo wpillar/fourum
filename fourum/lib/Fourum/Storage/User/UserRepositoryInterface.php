d7c8b127c99d50f0dc671003939109fa0f202aab
Will Pillar <will.pillar@graze.com>
2013-08-28 23:03:12 +0100

    Adding doc to UserRepositoryInterface and type hinting for array on create()

--- fourum/lib/Fourum/Storage/User/UserRepositoryInterface.php
+++ fourum/lib/Fourum/Storage/User/UserRepositoryInterface.php
@@ -1,10 +1,28 @@
 <?php namespace Fourum\Storage\User;
 
+/**
+ * User Repository Interface
+ *
+ * Interface for a User Repository
+ */
 interface UserRepositoryInterface
 {
+    /**
+     * Return all Users
+     */
     public function all();
 
+    /**
+     * Find User(s)
+     *
+     * @param  integer $id
+     */
     public function get($id);
 
-    public function create($input);
+    /**
+     * Create a User
+     *
+     * @param  array $input
+     */
+    public function create(array $input);
 }
