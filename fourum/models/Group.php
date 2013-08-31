ae0ffd67ff64ac731f269c80f1838d09308f272a
Will Pillar <will.pillar@graze.com>
2013-08-31 19:02:13 +0100

    Adding ocblock

--- fourum/models/Group.php
+++ fourum/models/Group.php
@@ -1,9 +1,17 @@
 <?php namespace Fourum\Models;
 
+/**
+ * Eloquent Group Model
+ */
 class Group extends Eloquent
 {
     protected $table = 'groups';
 
+    /**
+     * Get Users that are a member of this Group
+     *
+     * @return array
+     */
     public function users()
     {
         return $this->belongsToMany('User');
