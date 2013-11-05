06dab95daaa38b5e10d6f619a407f6d3893f9362
Will Pillar <me@willpillar.com>
2013-11-05 23:41:11 +0000

    Correcting a link url in forums.sidebar

--- public/themes/admin/default/views/forums/sidebar.php
+++ public/themes/admin/default/views/forums/sidebar.php
@@ -1,4 +1,4 @@
 <ul class="nav fourum-admin-sidenav"  data-spy="affix" data-offset-top="65">
-  <li class="active"><a href="#">Forums</a></li>
+  <li class="active"><a href="<?= url('admin/forums') ?>">Forums</a></li>
   <li><a href="#">Moderation</a></li>
 </ul>
