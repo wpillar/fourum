dd30d81aaaa43da2256e4edabef591b41cfc6a73
Will Pillar <will.pillar@graze.com>
2013-12-27 12:07:15 +0000

    Styling tweak to better separate categories on the homepage

--- public/themes/front/default/views/index.blade.php
+++ public/themes/front/default/views/index.blade.php
@@ -3,7 +3,7 @@
 
 <div class="row">
     <div class="col-md-12">
-    {{ $tree->html('<h4>%title%</h4><hr>', '<h4><a href="%url%">%title%</a></h4>') }}
+    {{ $tree->html('<hr><h4>%title%</h4><hr>', '<h4><a href="%url%">%title%</a></h4>') }}
     </div>
 </div>
 
